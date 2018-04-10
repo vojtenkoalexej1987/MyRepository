0e2b121ed52a8bd4e8c35282cf1a78b5020375a9
vojtenkoalexej1987 <36764905+vojtenkoalexej1987@users.noreply.github.com>
2018-04-10 13:37:42 +0300

    ----6

--- long-map/src/main/java/de/comparus/opensource/longmap/MyClassAny.java
+++ long-map/src/main/java/de/comparus/opensource/longmap/MyClassAny.java
@@ -2,6 +2,6 @@ package de.comparus.opensource.longmap;
 
 public class MyClassAny {
     public static void main(String[] args) {
-        System.out.println("22222");
+        System.out.println("33333");
     }
 }
