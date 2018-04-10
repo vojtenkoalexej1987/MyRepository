3fd326277a448964db24b69929a946dcb69e960f
voitenko <o.voitenko@comparus.de>
2018-04-10 12:09:59 +0300

    commit 7 branch 4

--- long-map/src/main/java/de/comparus/opensource/longmap/MyClass.java
+++ long-map/src/main/java/de/comparus/opensource/longmap/MyClass.java
@@ -2,6 +2,6 @@ package de.comparus.opensource.longmap;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("6");
+        System.out.println("7");
     }
 }
