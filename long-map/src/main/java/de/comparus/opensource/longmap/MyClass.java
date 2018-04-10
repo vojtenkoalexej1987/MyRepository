7cba0cad88b3cce5f405d9a36851bd583b1e13d9
voitenko <o.voitenko@comparus.de>
2018-04-10 12:35:55 +0300

    new 1

--- long-map/src/main/java/de/comparus/opensource/longmap/MyClass.java
+++ long-map/src/main/java/de/comparus/opensource/longmap/MyClass.java
@@ -2,6 +2,6 @@ package de.comparus.opensource.longmap;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("9");
+        System.out.println("new 1");
     }
 }
